1fe58bc8d70beb0a408f30d0bdefb69df82d0fc3
calebjore <cjore1@mailinator.com>
2017-04-15 08:10:23 -0400

    still trying...

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
   get 'sightings/index'
   resources :articles do
     resources :comments
   end
   root 'sightings#index'
+=======
+  get 'sightings' => 'pages#home'
+>>>>>>> parent of a4bd459... Made Rails Work
 =======
   get 'sightings' => 'pages#home'
 >>>>>>> parent of a4bd459... Made Rails Work
